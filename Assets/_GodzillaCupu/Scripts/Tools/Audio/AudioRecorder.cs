49a2624955ec2cbd087f7f7fb0ab52466fd8fc60
GodzillaCupu <prasetyo.djufri@gmail.com>
2025-04-25 03:51:32 +0700

    Craete Get Microphone method

--- Assets/_GodzillaCupu/Scripts/Tools/Audio/AudioRecorder.cs
+++ Assets/_GodzillaCupu/Scripts/Tools/Audio/AudioRecorder.cs
@@ -5,5 +5,35 @@ using UnityEngine;
 
 public class AudioRecorder : MonoBehaviour
 {
-   
+    [Header("Clips")]
+    [Serializefield] private AudioClip _recordedClip;
+
+    [Spave(5f)]
+    [SerializeField] private AudioSource _audioSource;
+
+    [Header("Microphone"),Space (5f)]
+    [SerializedField] private Microphone _currentMicrophone;
+    [SerializedField] private int _selectedMicIndex = 0;
+    [SerializedField] private string _microphoneName;
+
+    void OnEnable()
+    {
+        GetMicrophoneDevices();
+    }
+
+    private void GetMicrophoneDevices()
+    {
+        if(Microphone.devices.Length <= 0)
+        {
+            Debug.LogError("No microphone devices found.");
+            return;
+        }
+
+        Debug.Log("Microphone devices found: " + Microphone.devices.Length);
+        for (int i = 0; i < Microphone.devices.Length; i++)
+            Debug.Log("Microphone " + i + ": " + Microphone.devices[i]);
+
+        _currentMicrophone = Microphone.devices[_selectedMicIndex];
+        _micName = _currentMicrophone.name;
+    }
 }
