36538e99bf5c51c6a62ec4000bfe89019bd173a6
GodzillaCupu <prasetyo.djufri@gmail.com>
2025-04-26 00:24:58 +0700

    Create broadcaster for general pourpose with event in it

--- Assets/_GodzillaCupu/Scripts/Tools/Update System/UpdateBroadcaster.cs
+++ Assets/_GodzillaCupu/Scripts/Tools/Update System/UpdateBroadcaster.cs
@@ -5,7 +5,7 @@ public static class UpdateBroadcaster
 {
     #region Update
     [Header("Update")]
-    public  static List<IUpdateObserver> _updateObservers = new List<IUpdateObserver>();
+    public static List<IUpdateObserver> _updateObservers = new List<IUpdateObserver>();
     public static List<IUpdateObserver> _pendingUpdateObservers = new List<IUpdateObserver>();
     public static int _currentUpdateIndex;
 
@@ -20,8 +20,17 @@ public static class UpdateBroadcaster
     public static void Update()
     {
         if (_updateObservers.Count > 0)
-            foreach (var observer in _updateObservers)
+        {
+            for (_currentUpdateIndex = _updateObservers.Count - 1; _currentUpdateIndex >= 0; _currentUpdateIndex--)
+            {
+                var observer = _updateObservers[_currentUpdateIndex];
+                if (observer == null) continue;
                 observer.OnObservedUpdate();
+            }
+
+            _updateObservers.AddRange(_pendingUpdateObservers);
+            _pendingUpdateObservers.Clear();
+        }
     }
     #endregion
 
@@ -42,14 +51,23 @@ public static class UpdateBroadcaster
     public static void FixedUpdate()
     {
         if (_fixedUpdateObservers.Count > 0)
-            foreach (var observer in _fixedUpdateObservers)
+        {
+            for (_currentFixedUpdateIndex = _fixedUpdateObservers.Count - 1; _currentFixedUpdateIndex >= 0; _currentFixedUpdateIndex--)
+            {
+                var observer = _fixedUpdateObservers[_currentFixedUpdateIndex];
+                if (observer == null) continue;
                 observer.OnObservedFixedUpdate();
+            }
+
+            _fixedUpdateObservers.AddRange(_pendingFixedUpdateObservers);
+            _pendingFixedUpdateObservers.Clear();
+        }
     }
     #endregion
 
     #region Late Update
     [Header("Late Update")]
-    public static List<ILateUpdateObserver> _lateUpdateObservers = new List<ILateUpdateObserver>(); 
+    public static List<ILateUpdateObserver> _lateUpdateObservers = new List<ILateUpdateObserver>();
     public static List<ILateUpdateObserver> _pendingLateUpdateObservers = new List<ILateUpdateObserver>();
     public static int _currentLateUpdateIndex;
 
@@ -64,8 +82,17 @@ public static class UpdateBroadcaster
     public static void LateUpdate()
     {
         if (_lateUpdateObservers.Count > 0)
-            foreach (var observer in _lateUpdateObservers)
+        {
+            for (_currentLateUpdateIndex = _lateUpdateObservers.Count - 1; _currentLateUpdateIndex >= 0; _currentLateUpdateIndex--)
+            {
+                var observer = _lateUpdateObservers[_currentLateUpdateIndex];
+                if (observer == null) continue;
                 observer.OnObservedLateUpdate();
+            }
+
+            _lateUpdateObservers.AddRange(_pendingLateUpdateObservers);
+            _pendingLateUpdateObservers.Clear();
+        }
     }
     #endregion
 }
